7de3eb842a547adc309d6c4dd302b8ef4fbe0ccc
Abubakar Abdullahi <ajanifiles90@gmail.com>
2026-01-27 11:22:09 +0800

    - Added helper script to read sql file

--- plugins/helper.py
+++ plugins/helper.py
@@ -11,3 +11,10 @@ def derive_folder_from_filename(filename: str) -> str:
 def get_filename_from_url(url: str) -> str:
     path = urlparse(url).path
     return os.path.basename(path)
+
+
+def execute_sql(sql_tbl_path:str):
+    with open(sql_tbl_path, "r") as file:
+        create_ext_tbl_sql = file.read()
+
+    return create_ext_tbl_sql
